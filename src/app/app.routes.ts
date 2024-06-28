1918bd4fd038ed0dbc943a3128c24fba545bfdd5
Ramaz Borchashvili <ramazborchashvili00@gmail.com>
2024-06-28 17:03:34 +0400

    add form and header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import {
+  Task1Component,
+  Task2Component,
+  Task3Component,
+  Task4Component,
+  Task5Component,
+} from './tasks';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Task1Component,
+  },
+  {
+    path: 'task-1',
+    component: Task1Component,
+  },
+  {
+    path: 'task-2',
+    component: Task2Component,
+  },
+  {
+    path: 'task-3',
+    component: Task3Component,
+  },
+  {
+    path: 'task-4',
+    component: Task4Component,
+  },
+  {
+    path: 'task-5',
+    component: Task5Component,
+  },
+];
--- src/app/tasks/task-1/task-1.component.ts
+++ src/app/tasks/task-1/task-1.component.ts
@@ -1,12 +1,127 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-task-1',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './task-1.component.html',
-  styleUrl: './task-1.component.scss'
+  styleUrl: './task-1.component.scss',
 })
 export class Task1Component {
+  myForm: FormGroup = new FormGroup({
+    job: new FormArray([
+      new FormGroup({
+        companyName: new FormControl<string | undefined | null>(
+          null,
+          Validators.required
+        ),
+        companySiteUrl: new FormControl<string | undefined | null>(
+          null,
+          Validators.required
+        ),
+        companyDescription: new FormControl<string | undefined | null>(
+          null,
+          Validators.required
+        ),
+        positions: new FormArray([
+          new FormGroup({
+            position: new FormControl<string | undefined | null>(
+              null,
+              Validators.required
+            ),
+            level: new FormControl<string | undefined | null>(
+              null,
+              Validators.required
+            ),
+            description: new FormControl<string | undefined | null>(
+              null,
+              Validators.required
+            ),
+          })
+        ])
+      })
+    ])
+  });
 
+  addJob() {
+    const frmgroup: FormGroup = new FormGroup({
+        companyName: new FormControl<string | undefined | null>(
+          null,
+          Validators.required
+        ),
+        companySiteUrl: new FormControl<string | undefined | null>(
+          null,
+          Validators.required
+        ),
+        companyDescription: new FormControl<string | undefined | null>(
+          null,
+          Validators.required
+        ),
+        positions: new FormArray([
+          new FormGroup({
+            position: new FormControl<string | undefined | null>(
+              null,
+              Validators.required
+            ),
+            level: new FormControl<string | undefined | null>(
+              null,
+              Validators.required
+            ),
+            description: new FormControl<string | undefined | null>(
+              null,
+              Validators.required
+            ),
+          })
+        ])
+  });
+
+    (<FormArray>this.myForm.get('job')).push(frmgroup);
+  }
+  deleteJob(index: number) {
+    const frmArray = <FormArray>this.myForm.get('job');
+    frmArray.removeAt(index);
+  }
+
+  addPosition(i: number) {
+    const frmgroup: FormGroup = new FormGroup({
+      position: new FormControl<string | undefined | null>(
+        null,
+        Validators.required
+      ),
+      level: new FormControl<string | undefined | null>(
+        null,
+        Validators.required
+      ),
+      description: new FormControl<string | undefined | null>(
+        null,
+        Validators.required
+      ),
+    });
+      (<FormArray>this.myForm.get('job')?.value[i]?.positions).push(frmgroup);
+
+  }
+
+  deletePosition(jobIndex: number, index: number) {
+    const frmgroup = <FormArray>this.myForm.get('job')?.value[jobIndex]?.value.positions;
+    frmgroup.removeAt(index);
+  }
 }
